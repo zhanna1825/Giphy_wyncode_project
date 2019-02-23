74db28e3b0bb209f34f58fb30ec1a5fd3c3a51bd
Ed <ergonzalezpmp@gmail.com>
2019-02-23 09:37:03 -0500

    fixed indentation

--- src/Search.js
+++ src/Search.js
@@ -4,39 +4,35 @@ import Giph from './Giph.js';
 
 class Search extends Component {
 
-    constructor() {
-        super();
-        this.state = { items: [] , value: '' };
-
-        this.handleChange = this.handleChange.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { items: [] , value: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
 	fetchResults(props)
 	{
 		console.log(this.state.value);
-       fetch(`https://api.giphy.com/v1/gifs/search?api_key=X1bcZ3m4WswszHaa45Ws9uWklW6FdFN6&limit=20&offset=0&rating=G&lang=fr&q=`+this.state.value)
-           .then(result=>result.json())
-             .then(json =>{
-           	                this.setState( {items : json.data} )
-                           }
-           	       )
-	  }
-
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=X1bcZ3m4WswszHaa45Ws9uWklW6FdFN6&limit=20&offset=0&rating=G&lang=fr&q=`+this.state.value)
+      .then(result=>result.json())
+      .then(json =>{
+         this.setState( {items : json.data} )
+      } 
+    )
+	}
 
 	handleChange(event) {
        this.setState({value: event.target.value});
     }
 
-    render() {
-
+  render() {
     return (
       <div>
-         <input type="text" placeholder="Search..." id="search" value={this.state.value} onChange={this.handleChange}/>
-         <div className="block">
+        <input type="text" placeholder="Search..." id="search" value={this.state.value} onChange={this.handleChange}/>
+        <div className="block">
           <button type="submit" value="Submit" onClick={() => {this.fetchResults(this.state.searchQuery)}}>Submit</button>
-         </div>
-         <Giph items={this.state.items}/>
-
+        </div>
+        <Giph items={this.state.items}/>
       </div>
     );
   }
